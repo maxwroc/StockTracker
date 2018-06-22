cd5faefac445eeed324c4b47945202e73e82cb46
maxwroc <max.chodorowski@gmail.com>
2018-06-22 23:58:28 +0100

    Dongrade mongoose; index from file

--- src/controllers/main.controller.ts
+++ src/controllers/main.controller.ts
@@ -1,10 +1,24 @@
 
+
+const mongoose = require("mongoose");
+
+let Schema = mongoose.Schema;
+
+var StockType = new Schema({
+    name: String,
+    symbol: String
+});
+
+// Compile model from schema
+var StockTypeModel = mongoose.model('StockType', StockType);
+
+
 export class MainController {
     constructor(app: any, private database: any) {
-        app.get("/", conn => this.getIndex());
+        app.get("/", conn => this.getIndex(conn));
     }
 
-    private getIndex(): string {
-        return "Hello!!!";
+    private getIndex(conn: any): void {
+        conn.file("src/views/index.html");
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,16 @@ const controllers = [MainController, StockController];
 let app = mach.stack();
 app.use(mach.logger);
 
-let database = null;
+//Set up default mongoose connection
+let mongoDB = 'mongodb://192.168.2.103:27017/my_database';
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+let database = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+database.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 controllers.forEach(c => new c(app, database));
 
