747dee05da1f4a792323eb61e09c4c48c1af9d40
maxwroc <max.chodorowski@gmail.com>
2018-06-22 09:22:57 +0100

    nodemon

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 ///<reference path="shared.d.ts" />
 
 import { getProviders } from "./providers/provider";
-let mach = require("mach");
+import mach from "mach";
 
 let providers = getProviders("PL");
 
 var app = mach.stack();
 app.use(mach.logger);
+app.get("/", conn => {
+  return "hello preschool!!!"
+});
 app.get('/stock/:id', conn => {
   var id = conn.params.id;
 
