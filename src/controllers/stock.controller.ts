deff5f7096f9820fee2116e256a5e12f4b3cd4f5
maxwroc <max.chodorowski@gmail.com>
2018-06-27 15:47:56 +0100

    Debug support in VSCode

--- src/controllers/stock.controller.ts
+++ src/controllers/stock.controller.ts
@@ -5,12 +5,17 @@ import { getProviders } from "../providers/provider";
 export class StockController {
     constructor(app: any, database: any) {
         app.get('/stock/:symbol', conn => this.getStock(conn, conn.params.symbol));
+        app.post('/stock', conn => this.addStock(conn, conn.params.symbol));
     }
 
     private getStock(conn: any, symbol: string): Promise<IStockData> {
         let providers = getProviders("PL");
         return providers[0].getData(symbol)
                   .then(s => conn.json(200, s))
+    }
+
+    private addStock(conn:any, name: string) {
+
     }
 }
 
